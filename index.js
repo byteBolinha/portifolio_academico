0a7e63fee09f6e1cd34ccd85179168d3a184cda3
Ana Luisa <anadrvv@gmail.com>
2026-05-15 13:57:52 -0300

    instalei o cors para liberar o frotend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./src/config/db')
 const swaggerUi = require('swagger-ui-express')
 const swaggerSpec = require('./src/config/swagger')
@@ -9,6 +10,10 @@ const app = express();
 const authRoutes = require('./src/routes/auth.routes')
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:5173" 
+}));
 const injectDb = require('./src/middleware/db.middleware');
 app.use(injectDb);
 app.use('/auth', authRoutes);
