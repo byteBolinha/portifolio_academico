3c9529fa28a355eb20c93013a46cde61143bb7cd
Ana Luisa <anadrvv@gmail.com>
2026-05-19 21:11:35 -0300

    testes

--- src/middleware/jwt.middleware.js
+++ src/middleware/jwt.middleware.js
@@ -5,30 +5,66 @@ const Auth = (req, res, next) => {
     const role = (process.env.MOCK_ROLE || "ADMIN").toUpperCase();
 
     const users = {
-      ADMIN: {
-        id: 1,
-        roles_id: 1,
-        role: "ADMIN",
-        permissions: [
-          "CRIAR_COMPETENCIA",
-          "READ_ALL",
-          "LIBERAR_CUSTOMIZACAO",
-          "FLAG_PREENCHIDO",
-          "FLAG_AVALIADO_COORD",
-          "FLAG_AVALIADO_GESTAO",
-          "FLAG_CANVAS_INTEGRATION",
-          "MANAGE_LINKS_DRIVE",
-          "DELET_DOCUMENT",
-        ],
-      },
+  ADMIN: {
+    id: 1,
+    roles_id: 1,
+    role: "ADMIN",
+    permissions: [
+      "CRIAR_CURSO",
+      "CRIAR_COMPETENCIA",
+      "READ_ALL",
+      "LIBERAR_CUSTOMIZACAO",
+      "FLAG_PREENCHIDO",
+      "FLAG_AVALIADO_COORD",
+      "FLAG_AVALIADO_GESTAO",
+      "FLAG_CANVAS_INTEGRATION",
+      "MANAGE_LINKS_DRIVE",
+      "MANAGE_PERMISSIONS",
+      "CRIAR_TIPO_DE_DOCUMENTO",
+      "DELET_DOCUMENT",
+    ],
+  },
 
-      PROFESSOR: {
-        id: 2,
-        roles_id: 2,
-        role: "PROFESSOR",
-        permissions: ["READ_ALL"],
-      },
-    };
+  PROFESSOR: {
+    id: 2,
+    roles_id: 2,
+    role: "PROFESSOR",
+    permissions: [
+      "READ_ALL",
+      "FLAG_PREENCHIDO",
+    ],
+  },
+
+  COORDINATOR: {
+    id: 3,
+    roles_id: 3,
+    role: "COORDINATOR",
+    permissions: [
+      "READ_ALL",
+      "CRIAR_COMPETENCIA",
+      "FLAG_PREENCHIDO",
+      "FLAG_AVALIADO_COORD",
+    ],
+  },
+
+  NITE: {
+    id: 4,
+    roles_id: 4,
+    role: "NITE",
+    permissions: [
+      "CRIAR_CURSO",
+      "CRIAR_COMPETENCIA",
+      "READ_ALL",
+      "LIBERAR_CUSTOMIZACAO",
+      "FLAG_PREENCHIDO",
+      "FLAG_AVALIADO_COORD",
+      "FLAG_AVALIADO_GESTAO",
+      "FLAG_CANVAS_INTEGRATION",
+      "MANAGE_LINKS_DRIVE",
+      "CRIAR_TIPO_DE_DOCUMENTO",
+    ],
+  },
+};
 
     req.user = users[role];
 
