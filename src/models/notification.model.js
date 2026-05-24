8af8daeb0b2d17c6cdf8b5c383a72319a13269e2
Ana Luisa <anadrvv@gmail.com>
2026-05-24 20:31:23 -0300

    acrescentando infos nas notificações

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -24,14 +24,36 @@ class Notification {
   }
 
   static async findAll(db) {
-    const [result] = await db.query(`
-      SELECT *
-      FROM notifications
-      ORDER BY created_at DESC
-    `);
 
-    return result;
-  }
+  const [result] = await db.query(
+    `
+    SELECT
+      n.id_notification,
+      n.title,
+      n.message,
+      n.is_read,
+      n.created_at,
+
+      c.name_competency,
+
+      courses.name_courses
+
+    FROM notifications n
+
+    LEFT JOIN competency c
+      ON c.id_competency = n.competency_id
+
+    LEFT JOIN courses
+      ON courses.id_courses = c.course_id
+
+    ORDER BY n.created_at DESC
+    `
+  );
+
+  return result;
+}
+
+ 
 
   static async markAsRead(db, id) {
     return await db.query(
